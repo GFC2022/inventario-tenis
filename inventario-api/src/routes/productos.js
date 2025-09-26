8d87628b60157b748b74c49db51de11a042ea163
GFC2022 <100871016+GFC2022@users.noreply.github.com>
2025-09-26 22:53:26 +0000

    primer commitz

---
+++ inventario-api/src/routes/productos.js
@@ -0,0 +1,16 @@
+import express from "express";
+import pool from "../config/db.js";
+
+const router = express.Router();
+
+// Endpoint de prueba: obtener todos los productos
+router.get("/", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM productos");
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+export default router;
